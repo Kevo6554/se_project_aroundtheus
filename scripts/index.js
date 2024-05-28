b5013e87f8f232a8886de391b9cda26c6e2a5dda
kevinwitherspoon <kevofbe6@icloud.com>
2024-05-28 16:40:59 -0700

    Updating Project

--- scripts/index.js
+++ scripts/index.js
@@ -27,3 +27,16 @@ const initialCards = [
 ];
 
 console.log(initialCards);
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+profileEditModal.classList.remove("modal_opened");
+const profileCloseButton = document.querySelector("#profile-close-button");
+
+profileCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_close");
+});
