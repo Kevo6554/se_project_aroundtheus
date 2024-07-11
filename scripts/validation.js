8e1d9c0957ff41c2510d91bf3baa7ff78f31ebcd
kevinwitherspoon <kevofbe6@icloud.com>
2024-07-10 21:13:20 -0700

    corrections to project

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,20 +1,58 @@
 console.log("hello from validation.js");
 // enabling validation by calling enableValidation()
 // pass all the settings on call
+function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMesssageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.add(inputErrorClass);
+  errorMesssageEl.textContent = inputEl.validationMessage;
+  errorMesssageEl.classList.add(errorClass);
+}
+
+
+
+function checkInputValidity(formEl, inputEl, options) {
+  if (!inputEl.validity.valid) {
+    return showInputError(formEl, inputEl, options);
+  }
+  hideInputError(formEl, inputEl, options);
+}
+
+functionhasInvalidInput(inputList) {
+    return inputList.every((inputEl) => inputEl.validity.valid)
+}
+
+function toggleButtonState(inputEls, submitButton, { inactiveButtonClass }) {
+
+  if (hasInvalidInput) {
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.disabled = true;
+  }
+  submitButton.classList.remove(inactiveButtonClass);
+ (submitButton.disabled = true);
+ return;
+} 
 
 function setEventListener(formEl, options) {
   const { inputSelector } = options;
+  const submitButton = formEl.querySelector(".modal__button");
   const inputEls = [...formEl.querySelectorAll(options.inputSelector)];
+  inputEls.forEach((inputEl) => {
+    inputEls.addEventListener("input", (e) => {
+      checkInputValidity(formEl, inputEl, options);
+      toggleButtonState(inputEls, submitButton, options);
+    });
+  });
 }
 function enableValidation(options) {
   const formEls = [...document.querySelectorAll(options.formSelector)];
+
   formEls.forEach((formEl) => {
-    formEl.addEventListener("submit"),
-      (e) => {
-        e.preventDefault();
-      };
+    formEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+
+    setEventListeners(formEl, options);
   });
-  setEventListener(formEl, options);
 }
 
 const config = {
